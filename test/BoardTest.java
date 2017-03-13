59ea5754c5c2ec262566a3f9e0cdb745485658d6
GwenYu <gwenyu517@gmail.com>
2017-03-12 23:37:00 -0400

    Just added a couple of tests to BoardTest

--- test/BoardTest.java
+++ test/BoardTest.java
@@ -22,4 +22,25 @@ public class BoardTest{
         board.placeTile(tile, location,0);
         board.printMap();
     }
+
+    @Test
+    public void notAvailableLocation() {
+        Tile tile = new Tile(Terrain.GRASSLANDS, Terrain.JUNGLE);
+        Location location = new Location(0,0);
+        board.placeTile(tile, location,0);
+
+        Location location2 = new Location(0,0);
+        assertFalse(board.isAnAvailableSpace(location2));
+    }
+
+    @Test
+    public void hexAtLocation(){
+        Tile tile = new Tile(Terrain.GRASSLANDS, Terrain.JUNGLE);
+        Location location = new Location(0,0);
+        board.placeTile(tile, location,0);
+
+        Tile tile2 = new Tile(Terrain.GRASSLANDS, Terrain.JUNGLE);
+        Location location2 = new Location(2,1);
+        assertFalse(board.hexExistsAtLocation(location2));
+    }
 }
