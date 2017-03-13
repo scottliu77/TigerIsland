22f331b05f4991bedda5dd1e977948eb7a322bf6
SlightlyUnorthodox <gerts.dax1@gmail.com>
2017-03-12 20:26:31 -0400

    Changed timer class to allow tests to run

--- src/Timer.java
+++ src/Timer.java
@@ -1,6 +1,12 @@
 public class Timer {
 
-    public Timer() {
-        System.out.println("This timer exists");
+    private int maxSeconds;
+
+    public Timer(int maxSeconds) {
+        this.maxSeconds = maxSeconds;
+    }
+
+    public int getMaxSeconds() {
+        return this.maxSeconds;
     }
 }
--- test/TimerTest.java
+++ test/TimerTest.java
@@ -4,15 +4,16 @@ import static org.junit.Assert.*;
 
 public class TimerTest {
 
-    Timer timer;
+    private final int MAXSECONDS = 10;
+    private Timer timer;
 
     @Before
     public void createTimer() {
-        this.timer = new Timer();
+        this.timer = new Timer(this.MAXSECONDS);
     }
 
     @Test
     public void testCanCreateTimer() {
-        assertTrue(this.timer != null);
+        assertTrue(timer != null);
     }
 }
