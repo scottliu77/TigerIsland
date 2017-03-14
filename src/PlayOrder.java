6bab1a757e904efcffb7298d470f8289938b24f0
GwenYu <gwenyu517@gmail.com>
2017-03-14 19:29:40 -0400

    created Turn class; no tests yet.

--- src/PlayOrder.java
+++ src/PlayOrder.java
@@ -5,15 +5,7 @@ public class PlayOrder {
     private int currentPlayer = 0;
 
     public ArrayList<Player> determinePlayOrder(ArrayList<Player> players){
-        int a = 0;
-        int b = players.size();
-        int c = 0;
-        for (int i = a; i < b; i++){
-            c = a - 1 + (int)(Math.random() * (b - a));
-            Player temp = players.get(a);
-            players.set(a, players.get(c));
-            players.set(c, temp);
-        }
+        Collections.shuffle(players);
         return players;
     }
 
