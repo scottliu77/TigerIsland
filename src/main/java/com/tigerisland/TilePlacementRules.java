d67fcb2c26b063cc043737ffa0d448858785ee65
SlightlyUnorthodox <gerts.dax1@gmail.com>
2017-03-17 05:44:59 -0400

    Designed framework for dynamic rule definition and checking

---
+++ src/main/java/com/tigerisland/TilePlacementRules.java
@@ -0,0 +1,8 @@
+package com.tigerisland;
+
+public final class TilePlacementRules {
+
+    public static void basicRule(Game game) {
+        System.out.print("Do nothing for now");
+    }
+}
---
+++ src/main/java/com/tigerisland/TotoroPlacementRules.java
@@ -0,0 +1,8 @@
+package com.tigerisland;
+
+public final class TotoroPlacementRules {
+
+    public static void basicRule(Game game) {
+        System.out.print("Do nothing for now");
+    }
+}
