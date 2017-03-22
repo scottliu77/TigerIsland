2e5fea9e2b316385a2383008d0f50f79d7e7849d
jjbarta <jjbarta@ufl.edu>
2017-03-21 21:59:15 -0400

    It was distracting to use the four different function for four different states of the board. It was more intention revealing to show how the functions individually operate on the same board to reveal how each of the functions work (even though in general it's best to seperate things.

--- src/test/java/com/tigerisland/TextGUITest.java
+++ src/test/java/com/tigerisland/TextGUITest.java
@@ -2,7 +2,6 @@ package com.tigerisland;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class TextGUITest {
 
@@ -11,69 +10,51 @@ public class TextGUITest {
     private Tile tile;
     private Tile tile2;
     private Tile tile3;
+    private Tile tile4;
     private Location location;
     private Location location2;
     private Location location3;
+    private Location location4;
 
     @Before
     public void createBasicMocks() {
-        this.textGUI = new TextGUI();
         this.board = new Board();
         this.tile = new Tile(Terrain.GRASSLANDS, Terrain.JUNGLE);
         this.tile2 = new Tile(Terrain.LAKE, Terrain.ROCKY);
         this.tile3 = new Tile(Terrain.LAKE, Terrain.GRASSLANDS);
-        this.location = new Location(0,0);
+        this.tile4 = new Tile(Terrain.JUNGLE, Terrain.JUNGLE);
+        this.location = new Location(0, 0);
         this.location2 = new Location(-1, 0);
         this.location3 = new Location(-2, 0);
-    }
+        this.location4 = new Location(0, -1);
 
-    @Test
-    public void testCanCreateTextGUI() {
-        assertTrue(textGUI != null);
+        try {
+            board.placeTile(tile, location, 0);
+            board.placeTile(tile2, location2, 60);
+            board.placeTile(tile3, location3, 120);
+            board.placeTile(tile4, location4, 240);
+        } catch (InvalidMoveException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void testCanPrintPlacedHexLocations() {
-        try {
-            board.placeTile(tile, location, 0);
-        } catch (InvalidMoveException e) {
-            e.printStackTrace();
-        }
         TextGUI.printPlacedHexLocations(board.locationsOfPlacedHexes());
     }
 
     @Test
     public void testCanPrintEdgeSpaces() {
-        try {
-            board.placeTile(tile, location, 0);
-        } catch (InvalidMoveException e) {
-            e.printStackTrace();
-        }
         TextGUI.printEdgeSpaces(board.edgeSpaces);
     }
 
     @Test
     public void testCanPrintPlacedHexTiles() {
-        try {
-            board.placeTile(tile, location, 0);
-        } catch (InvalidMoveException e) {
-            e.printStackTrace();
-        }
         TextGUI.printPlacedHexTiles(board.hexesOfPlacedHexes());
     }
 
     @Test
     public void testCanPrintMap() {
-        try {
-            board.placeTile(tile, location, 0);
-            board.placeTile(tile2, location2, 60);
-            board.placeTile(tile3, location3, 120);
-        } catch (InvalidMoveException e) {
-            e.printStackTrace();
-        }
         TextGUI.printMap(board.locationsOfPlacedHexes(), board.edgeSpaces, board.hexesOfPlacedHexes());
     }
-
-
-
 }
